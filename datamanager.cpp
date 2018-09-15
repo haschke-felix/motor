77369e660b59cf3ab516f19deaae249e0841469e
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-09-16 01:34:17 +0200

    partially revert cleanup

--- datamanager.cpp
+++ datamanager.cpp
@@ -7,6 +7,7 @@ DataManager::DataManager(SPI *spi, unsigned int sampling_count) : spi_(spi), upd
 
 void DataManager::process()
 {
+	control_.setMaxBoostPWM(receive_.max_boost_pwm);
 	control_.process();
 	if (update_count_ && ++counter_ >= update_count_) {
 		counter_ = 0;
@@ -22,7 +23,6 @@ union intByte{
 void DataManager::receivedValues(const byte *data)
 {
 	receive_.max_boost_pwm = data[0];
-	control_.setMaxBoostPWM(receive_.max_boost_pwm);
 }
 
 int DataManager::byteArrayToInt(byte *ptr)
